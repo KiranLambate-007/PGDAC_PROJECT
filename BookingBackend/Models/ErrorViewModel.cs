29c92653dee45b5e91ac91942ed59fe96f3b8421
sharayu vanjari <sharayuvanjari@sharayus-MacBook-Air.local>
2025-08-02 14:32:51 +0530

    Added backend project files

---
+++ BookingBackend/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BookingBackend.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
