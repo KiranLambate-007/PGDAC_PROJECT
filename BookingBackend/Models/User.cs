dd8f761e8366c545e66df7cfa1f0217d6267ee9c
sharayu vanjari <vanjarisharayu@gmail.com>
2025-08-05 00:36:20 +0530

    user model updation

--- BookingBackend/Models/User.cs
+++ BookingBackend/Models/User.cs
@@ -5,11 +5,14 @@ namespace BookingBackend.Models;
 public class User
 {
     public int UserId { get; set; }
+    
     public string FullName { get; set; }
     public string Email { get; set; }
     public string Password { get; set; }
     public string PhoneNumber { get; set; }
     public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+    public string AadharCardNumber { get; set; } = string.Empty;
 
     public ICollection<Ticket> Tickets { get; set; }
     public ICollection<TicketTransfer> TransfersFrom { get; set; }
